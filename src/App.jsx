4a1d508a604c40ffe98809a82ab72fc3422af8f8
2596915059@qq.com <2596915059@qq.com>
2024-04-11 11:46:05 +0800

    feat: 更新部分代码

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
 // import { useState } from 'react'
 
 // import viteLogo from '/vite.svg'
-import {  Route,Routes,Link, Navigate  } from 'react-router-dom'
-import Popular from './pages/popular';
-import Battle from './pages/battle';
+import { Route, Routes, Link, Navigate } from 'react-router-dom'
+import Popular from './pages/popular'
+import Battle from './pages/battle'
 import './App.css'
 
 function App() {
-
   return (
-      <div>
-        <Link to="./popular">Popular</Link>
-        <Link to="./battle">Battle</Link>
-       <div className='view-box'>
-       <Routes>
-          <Route path="/popular" element={<Popular/>}></Route>
-          <Route path="/battle" element={<Battle/>}></Route>
-          <Route path="/" element={<Navigate to="/popular" />} />
-        </Routes>
-       </div>
+    <div className='app-box'>
+      <div className='nav-box'>
+        <Link to='./popular'>Popular</Link>
+        <Link to='./battle'>Battle</Link>
       </div>
+      <div className='view-box'>
+        <Routes>
+          <Route path='/popular' element={<Popular />}></Route>
+          <Route path='/battle' element={<Battle />}></Route>
+          <Route path='/' element={<Navigate to='/popular' />} />
+        </Routes>
+      </div>
+    </div>
   )
 }
 
--- src/pages/battle.jsx
+++ src/pages/battle.jsx
@@ -1,12 +1,91 @@
+import { useState } from 'react'
+import './battle.scss'
 function Battle() {
-    return (
-      <div>
-        <h1>battle</h1>
-        <p>This is the popular component</p>
-        <p>It has a header, content, and footer</p>
-      </div>
-    )
+  const [input1, setInput1] = useState('')
+  const [input2, setInput2] = useState('')
+  const [showInput1, setShowInput1] = useState(true)
+  const [showInput2, setShowInput2] = useState(true)
+  const [submittedInfo, setSubmittedInfo] = useState(null)
+
+  const handleInput1Change = event => {
+    setInput1(event.target.value)
   }
-  
-  export default Battle
-  
+
+  const handleInput2Change = event => {
+    setInput2(event.target.value)
+  }
+
+  const handleSubmit1 = () => {
+    console.log('123')
+    setSubmittedInfo(input1)
+    setShowInput1(false)
+  }
+
+  const handleSubmit2 = () => {
+    setSubmittedInfo(input2)
+    setShowInput2(false)
+  }
+
+  const handleCloseInfo = index => {
+    if (index == 1) {
+      setShowInput1(true)
+    } else if (index == 2) {
+      setShowInput2(true)
+    } else {
+      setShowInput1(true)
+      setShowInput2(true)
+    }
+  }
+  return (
+    <div className='battle-box'>
+      <h1>操作流程</h1>
+      <div className='battle-process'>
+        <div>第一步：选择用户</div>
+        <div>第二步：按下battle按钮</div>
+        <div>第三步：查看结果</div>
+      </div>
+      <div className='battle-operate'>
+        <div className='battle-input-box'>
+          <div className='person-num'>Player One</div>
+          {showInput1 && (
+            <div>
+              <input type='text' value={input1} onChange={handleInput1Change} />
+              <button onClick={handleSubmit1}>提交1</button>
+            </div>
+          )}
+          {!showInput1 && (
+            <div>
+              <p>{input1}</p>
+              <button onClick={handleCloseInfo(1)}>关闭</button>
+            </div>
+          )}
+        </div>
+        <div className='battle-input-box'>
+          <div className='person-num'>Player Two</div>
+          {showInput2 && (
+            <div>
+              <input type='text' value={input2} onChange={handleInput2Change} />
+              <button onClick={handleSubmit2}>提交2</button>
+            </div>
+          )}
+          {!showInput2 && (
+            <div>
+              <p>{input2}</p>
+              <button onClick={handleCloseInfo(2)}>关闭</button>
+            </div>
+          )}
+        </div>
+      </div>
+      <div className='battle-button'>
+        {!showInput2 && !showInput2 && (
+          <div>
+            <p>{submittedInfo}</p>
+            <button onClick={handleCloseInfo(3)}>关闭</button>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Battle
