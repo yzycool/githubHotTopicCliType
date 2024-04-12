bda687fe56a1711961d835f956663526d4eb77fa
2596915059@qq.com <2596915059@qq.com>
2024-04-12 14:09:19 +0800

    feat:修改eslint配置

--- src/until/fetch.js
+++ src/until/fetch.js
@@ -2,17 +2,15 @@
 
 async function fetchData(baseUrl, url, options = {}) {
   try {
-    const response = await fetch(`${baseUrl}${url}`, options)
+    const response = await fetch(`${baseUrl}${url}`, options);
     if (!response.ok) {
-      alert(`HTTP error! status: ${response.status}`)
-      throw new Error(`HTTP error! status: ${response.status}`)
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
-    const data = await response.json()
-    return data
+    const data = await response.json();
+    return data;
   } catch (error) {
-    alert(`HTTP error! status: ${error}`)
-    console.error('Fetch Error:', error)
-    throw error
+    console.error('Fetch Error:', error);
+    throw error;
   }
 }
-export default fetchData
+export default fetchData;
